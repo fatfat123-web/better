432bdd63326f849faa35f81862ad7327a86146ac
fatfat123-web <653694078@qq.com>
2020-06-04 00:45:43 +0800

    321

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,83 +1,79 @@
 <template>
-    <div class="wrapper" ref="wrapper">
+    <div class="wrapper">
         <ul class="content">
-            <li style="background:burlywood ">1</li>
-            <li style="background:red ">2</li>
-            <li style="background:orangered ">3</li>
-            <li style="background:chartreuse ">4</li>
-            <li style="background:violet ">5</li>
-            <li style="background:cornflowerblue ">6</li>
-
+            <li>第一屏</li>
+            <li>第二屏</li>
+            <li>第三屏</li>
         </ul>
     </div>
 </template>
 
 <script>
     import BScroll from 'better-scroll'
+
     export default {
         name: "index",
-        data(){
-            return{
+        data() {
+            return {
                 scroll: true,
-                // background: [
-                //     'burlywood', 'red', 'orangered','violet','cornflowerblue',
-                // ],
-                options:{
-                    scrollY: true,
-                    snap: {
-                        loop: false, // 开启循环播放
-                        stepX: 200, // 每页宽度为 200px
-                        stepY: 100, // 每页高度为 100px
-                        threshold: 0.3, // 滚动距离超过宽度/高度的 30% 时切换图片
-                        speed: 400 // 切换动画时长 400ms
-                    }
-                }
+
+
+
 
             }
         },
-        methods:{
-            scrollTo(item){
-              console.log(item)
-
-            },
-
-        },
+        methods: {},
         mounted() {
 
+            new BScroll('.wrapper', {
+                       click: true,
+                snap: {  // 滑块切换的一些配置
 
-            let scroll = new BScroll('.wrapper',{
-                scrollY: true,
-                click: true
-            })
+                    threshold: 0.3,  // 滑动切换到超过一半时切换到下一屏
+                    stepY: window.innerHeight  // 纵向切换距离为窗口高度
+                }
+            });
 
-            const options = {
-                scrollY: true // 因为scrollY默认为true，其实可以省略
-            }
-
-            // options.pullDownRefresh = {
-            //     threshold: 50, // 当下拉到超过顶部 50px 时，触发 pullingDown 事件
-            //     stop: 20 // 刷新数据的过程中，回弹停留在距离顶部还有 20px 的位置
-            // }
-
-            setTimeout(() => {
-                this.scroll = new BScroll(this.$refs.wrapper, options)
-            }, 20)
         }
     }
 </script>
 
 <style scoped>
-    .wrapper{
+    .wrapper {
         background: salmon;
         position: fixed;
         left: 0;
         top: 0;
         bottom: 0;
         width: 100%;
-        overflow:hidden;
+        overflow: hidden;
     }
-    .content li{
-        height: 100vh;
 
+    .content {
+        margin: 0;
+        padding: 0;
+    }
+
+    .content li {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        list-style: none;
+        font-size: 50px;
+        font-weight: bold;
+        color: #fff;
+    }
+
+    .content li:nth-child(1) {
+        background: #f00;
+    }
+
+    .content li:nth-child(2) {
+        background: #0f0;
+    }
+
+    .content li:nth-child(3) {
+        background: #00f;
     }
 </style>
