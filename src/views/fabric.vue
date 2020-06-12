4be192bf3e5b14b681ad6ba3087ca4b1ffa3d56b
Administrator <653694078@qq.com>
2020-06-12 17:35:36 +0800

    找到点问题

--- src/views/fabric.vue
+++ src/views/fabric.vue
@@ -2,7 +2,7 @@
     <div>
 
         <canvas id="main"  ref="cvs" style="border:1px solid #c3c3c3;">
-            <img src="../assets/images/1.jpg" id="img1"> </canvas>
+            <img src="../assets/images/1.jpg" id="img" style="width:50%;height: auto;"> </canvas>
 
     </div>
 </template>
@@ -21,31 +21,48 @@
                 canvas: null,
             }
         },
-        methods: {},
+        methods: {
+            initialize(){
+                const _this = this;
+                //设置舞台
+                this.canvas = new fabric.Canvas("main", {
+                    width:window.innerWidth,
+                    height:window.innerHeight,
+                    backgroundColor: "rgb(100,100,200)",
+                });
+
+                //获取id
+                this.imgElement = document.getElementById('img');
+                console.log(this.imgElement)
+
+                this.imgInstance = new fabric.Image(this.imgElement, {  //设置图片的样式
+                    left: 50,
+                    top: 50,
+                    scaleX: 0.5,
+                    scaleY: 0.5,
+                    // width: this.imgElement.style.width,
+                    // height: '50%',
+                    // height: window.innerHeight,
+                    angle: 0,//设置图形顺时针旋转角度
+                });
+
+                fabric.Image.fromURL('../assets/images/1.jpg', (oImg) =>  {
+                    oImg.scale(0.1);//图片缩小10倍
+                    this.canvas.add(oImg);
+                    console.log(oImg)
+                });
+
+
+                console.log(new fabric.Image)
+                console.log(this.imgElement.style.height)
+                this.canvas.add(this.imgInstance);
+
+            },
+
+        },
         mounted() {
-            const _this = this;
-            this.canvas = new fabric.Canvas("main", {
+            this.initialize()
 
-                width:window.innerWidth,
-                height:window.innerHeight,
-
-                backgroundColor: "rgb(100,100,200)",
-
-            });
-
-            fabric.Image.fromURL('../assets/images/1.jpg', function (oImg) {
-                oImg.scale(0.1);//图片缩小10倍
-                _this.canvas.add(oImg);
-            });
-            this.imgElement = document.getElementById('img1');
-            this.imgInstance = new fabric.Image(this.imgElement, {  //设置图片位置和样子
-                left: 100,
-                top: 100,
-                width: 200,
-                height: 100,
-                angle: 30//设置图形顺时针旋转角度
-            });
-            this.canvas.add(this.imgInstance);
         }
     }
 </script>
